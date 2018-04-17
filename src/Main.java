685ed3b991d730aabb63339192007fc76b7d8edc
Marco Barnà <34555572+MarcoBarna@users.noreply.github.com>
2018-04-17 17:36:47 +0200

    Delete Main.java

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("HELLO");
-        System.out.println("NUOVA LIBRERIA AVANZATA DI ORDINAMENTO");
-    }
-}
