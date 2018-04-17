e81b093b7f0315afb58d8e7f0a3a9ab709365e31
Marco Barnà <34555572+MarcoBarna@users.noreply.github.com>
2018-04-17 17:37:12 +0200

    Add files via upload

---
+++ it/unive/dais/po/ordinator/Main.java
@@ -0,0 +1,11 @@
+package it.unive.dais.po.ordinator;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("HELLO");
+        System.out.println("NUOVA LIBRERIA AVANZATA DI ORDINAMENTO");
+
+
+    }
+}
