2661871277c9b16d809816648cedd7fe0b0759c0
AlviseB <37024233+AlviseB@users.noreply.github.com>
2018-04-27 11:45:47 +0200

    Add files via upload

---
+++ it/unive/dais/po/ordinator/InsertionSort2.java
@@ -0,0 +1,107 @@
+package it.unive.dais.po.ordinator;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Iterator;
+
+public class InsertionSort2<T> implements Ordinator<T> {
+
+    private ArrayList<T> myArrayList;
+    private Comparator<T> myComparator;
+
+    private boolean sorted;
+
+    public InsertionSort2() {
+        myArrayList = new ArrayList<T>();
+        myComparator = null;
+        sorted = false;
+    }
+
+    public InsertionSort2(Comparator<T> comparator) {
+        this();
+        addComparator(comparator);
+    }
+
+    public InsertionSort2(Collection<T> collection) {
+        this();
+        addData(collection);
+    }
+
+    public InsertionSort2(Collection<T> collection, Comparator<T> comparator) {
+        this(comparator);
+        addData(collection);
+    }
+
+    @Override
+    public void addData(Collection<T> data) throws RuntimeException {
+        sorted = false;
+            if(myArrayList.addAll(data) == false)
+                throw new RuntimeException("addData(Collection.addAll) failed");
+    }
+
+    @Override
+    public void addData(T data) throws RuntimeException {
+        sorted = false;
+        if(myArrayList.add(data) == false)
+            throw new RuntimeException("addData(Collection.addAll) failed");
+    }
+
+    @Override
+    public Integer getSize() {
+        return myArrayList.size();
+    }
+
+    @Override
+    public void addComparator(Comparator<T> comparator) {
+        myComparator = comparator;
+    }
+
+    @Override
+    public void sort() throws NoComparatorFound {
+        Integer i, j;
+        if(myComparator == null)
+            throw new NoComparatorFound("You forgot to add a comparator");
+
+        for(j = 1; j < myArrayList.size(); j++) {
+            T key = myArrayList.get(j);
+
+            i = j - 1;
+
+            while(i >= 0 && myComparator.compare(myArrayList.get(i), key) > 0) {
+                myArrayList.set(i+1, myArrayList.get(i));
+                i--;
+            }
+
+            myArrayList.set(i + 1, key);
+        }
+        sorted = true;
+    }
+
+    @Override
+    public void sort(Comparator<T> comp) throws NoComparatorFound {
+        addComparator(comp);
+        sort();
+    }
+
+    @Override
+    public ArrayList<T> getArrayListCopy() {
+        return new ArrayList<T>(myArrayList);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return (sorted) ? myArrayList.iterator()
+            : new Iterator<T>() {
+                @Override
+                public boolean hasNext() {
+                    return false;
+                }
+
+                @Override
+                public T next() {
+                    return null;
+                }
+            };
+    }
+}
---
+++ it/unive/dais/po/ordinator/Ordinator2.java
@@ -0,0 +1,23 @@
+package it.unive.dais.po.ordinator;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Iterator;
+
+public interface Ordinator2<T> extends Iterable<T>{
+
+    public void addData(Collection<T> data);
+
+    public void addData(T data);
+
+    public Integer getSize();
+
+    public void addComparator(Comparator<T> comp);
+
+    public void sort() throws NoComparatorFound;
+
+    public void sort(Comparator<T> comp) throws NoComparatorFound;
+
+    public ArrayList<T> getArrayListCopy();
+}
