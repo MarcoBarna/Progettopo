092de5a344040cc6cd015b2781e91a1868180d61
lollobene <38067715+lollobene@users.noreply.github.com>
2018-05-19 12:36:34 +0200

    Ho aggiunto l'heapsort e un main di test, funzioina ma devo finire tutti i metodi e commentare bene

---
+++ it/unive/dais/po/ordinator/HeapTest.java
@@ -0,0 +1,31 @@
+package it.unive.dais.po.ordinator;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class HeapTest {
+
+    public static void main(String args[]) throws NoComparatorFound {
+
+        ArrayList<Integer> myList = new ArrayList<>();
+        myList.add(16);
+        myList.add(4);
+        myList.add(10);
+        myList.add(14);
+        myList.add(7);
+        myList.add(9);
+        myList.add(3);
+        myList.add(2);
+        myList.add(8);
+        myList.add(1);
+
+        System.out.println(myList);
+
+        HeapSort<Integer> myHeapSorter = new HeapSort<>();
+        myHeapSorter.addData(myList);
+        myHeapSorter.addComparator((Integer a, Integer b) -> (a.compareTo(b)));
+        myHeapSorter.sort();
+
+        System.out.println(myHeapSorter.getArrayListCopy());
+    }
+}
